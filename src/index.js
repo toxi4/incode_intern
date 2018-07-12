0051906f25ea2342b6e64354c569fc073ac130b8
toxi4 <xtoxi4x@gmail.com>
2018-07-12 21:42:48 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import 'babel-polyfill'
 import React from 'react'
-import ReactDom from 'react-dom'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './containers/App'
 
+const store = createStore( () => {}, {});
 
-ReactDom.render(
-  <App />,
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-)
+);
