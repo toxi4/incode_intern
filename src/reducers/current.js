56a2bc2106f76f0bea92abfd338d06cdf136ae5a
toxi4 <xtoxi4x@gmail.com>
2018-07-13 16:19:55 +0300

    w

--- src/reducers/current.js
+++ src/reducers/current.js
@@ -1,3 +1,9 @@
-export default function (state,action){
-	
+export default function (state=null,action){
+	switch (action.type){
+		case 'CLIENT_SELECTED':
+			return action.payload;
+			break;
+		default:
+			return state;
+	}
 }
