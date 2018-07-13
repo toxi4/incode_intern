523a0fd0ccbdadcc50e619b3c16eb230f6489447
toxi4 <xtoxi4x@gmail.com>
2018-07-13 16:06:11 +0300

    w

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
 export const choose =(client) => {
-	
+	return {
+		type: 'CLIENT_SELECTED',
+		payload: client
+	}
 }
--- src/containers/clientslist.js
+++ src/containers/clientslist.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {choose} from '../actions/index';
 
 class ClientsList extends Component{
 	showClients(){
